2f976b452b289e025296052abb137b75dcd3b0ca
Anthony Risch <rischthefisch@gmail.com>
2020-02-11 23:48:42 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+  attr_accessor :name, :roster, :grade
+  
+  
+  def initialize(name)
+    @name=name
+    @roster = {}
+  end
+  
+  def  add_student(name,grade)
+  	roster[grade] = []
+  	roster[grade] << name
+end
+  
+end
+school = School.new("Bayside High School")
+school.roster
+school.add_student("Zach Morris", 9)
