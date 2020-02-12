44422dde99daf115dc7d438d2448a43ac1e19d39
Anthony Risch <rischthefisch@gmail.com>
2020-02-12 00:21:13 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -22,4 +22,7 @@ end
 
 school = School.new("Bayside High School")
 school.roster
-school.add_student("Zach Morris", 9)
+school.add_student("AC Slater", 9)
+school.add_student("Kelly Kapowski", 10)
+school.add_student("Screech", 11)
+school.roster
