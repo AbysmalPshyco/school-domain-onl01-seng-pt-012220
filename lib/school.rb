e0b0ccac82d6ba97b9ddd67933ed281c1899de00
Anthony Risch <rischthefisch@gmail.com>
2020-02-12 00:46:43 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  attr_accessor :name, :roster, :grade
+  
+  
+  def initialize(name)
+    @name=name
+    @roster = {}
+  end
+  
+  def  add_student(name,year)
+  	roster[year] ||= []
+  	roster[year] << name
+ end
+
+def grade(year)
+  @roster[year]
+end
+
+def sort
+  roster.each {|key,value| value.sort!}
+
+ end
+ end
+ 
